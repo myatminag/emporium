b729680415bf1a55ee4e7fc506a56f76f0a33ad3
MyatMinAung <myatminaung.dev@gmail.com>
2024-03-01 10:57:52 +0630

    update: lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  '{packages,apps}/**/*.{js,ts,tsx}': [
-    'pnpm nx affected:lint --uncommitted --fix true',
-    'pnpm nx affected:test --uncommitted',
-    'pnpm nx format:write --uncommitted',
-  ],
+  '{apps,libs,tools}/**/*.{ts,tsx}': (files) => {
+    return `pnpm nx affected --target=typecheck --files=${files.join(',')}`;
+  },
   // '*.{ts,tsx}': () => ['pnpm nx affected --target=type-check'],
   // '*.{js,ts,tsx}': () => [
   //   'pnpm nx affected:lint --untracked=false --fix',
