353ffde79a44f56ec911697adba33613a5d09859
MyatMinAung <myatminaung.dev@gmail.com>
2024-03-05 08:43:08 +0630

    chore: remove backend service

---
+++ apps/admin/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default async function Index() {
+  return (
+    <p className="mt-10 text-center text-3xl font-semibold">Hello Admin</p>
+  );
+}
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  '{apps,packages}/(*)/**/*.{ts,tsx}': () => [
-    'pnpm nx affected --target=type-check',
-  ],
-  '{apps,packages}/(*)/**/*.{js,ts,jsx,tsx,json}': [
+  '{apps,packages}/*.{ts,tsx}': () => ['pnpm nx affected --target=type-check'],
+  '{apps,packages}/*.{js,ts,jsx,tsx,json}': [
     'pnpm nx affected:lint',
     'pnpm nx format:write',
   ],
