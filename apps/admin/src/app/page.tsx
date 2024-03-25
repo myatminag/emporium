aa61cc184950b4280b4299a98a9d8b1c884aef91
MyatMinAung <myatminaung.dev@gmail.com>
2024-03-25 23:01:51 +0630

    wip: login form design

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,43 +1,18 @@
-import Image from 'next/image';
 import { MainLogo } from 'packages/ui/src';
 import LoginForm from '../components/login/LoginForm';
 
 export default async function Index() {
   return (
-    <div className="relative flex h-screen w-full items-center justify-center">
-      <Image
-        src={'/left-circle.png'}
-        alt="left-circle"
-        width={200}
-        height={200}
-        className="absolute left-0 top-1/2 -translate-y-1/2"
-      />
-      <Image
-        src={'/top-right.png'}
-        alt="left-circle"
-        width={300}
-        height={300}
-        className="absolute right-0 top-0"
-      />
-      <Image
-        src={'/bottom-right.png'}
-        alt="left-circle"
-        width={300}
-        height={300}
-        className="absolute bottom-0 right-0"
-      />
-      <div className="flex min-w-[500px] flex-col">
-        <div className="mb-4 flex items-center justify-center gap-4">
-          <MainLogo />
-          <h3 className="text-primary text-xl font-semibold">Emporium</h3>
-        </div>
-        <div className="bg-secondary-100 flex h-auto w-full flex-col gap-y-5 rounded-xl px-8 py-4">
-          <h1 className="text-center text-xl font-medium">Sign In</h1>
-          <LoginForm />
-        </div>
-         deserunt ab ad sequi tempore repellendus ex sed dicta eos aut excepturi animi sint. Ex, architecto?
+    <div className="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <MainLogo className="mx-auto size-10" />
+        <h2 className="mt-10 text-center text-2xl font-semibold leading-9 tracking-tight text-neutral-700">
+          Sign in to your account
+        </h2>
+      </div>
+      <div className="mt-10 lg:mx-auto lg:w-full lg:max-w-sm">
+        <LoginForm />
       </div>
     </div>
   );
 }
-
--- apps/admin/src/components/login/LoginForm.tsx
+++ apps/admin/src/components/login/LoginForm.tsx
@@ -1,46 +1,67 @@
 'use client';
-import { InfoIcon, PhoneIcon, Button, DashboardIcon } from 'packages/ui/src';
+
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import { InfoIcon, Button, PhoneIcon } from 'packages/ui/src';
+
 const loginValidationSchema = z.object({
-  phoneNumber: z.string().min(1, { message: 'Phone number is required' }),
+  email: z.string().min(1, { message: 'Email number is required' }),
   password: z.string().min(8, { message: 'Password is required' }),
 });
+
 const MyComponent = () => {
   const form = useForm<z.infer<typeof loginValidationSchema>>({
     resolver: zodResolver(loginValidationSchema),
     defaultValues: {
-      phoneNumber: '',
+      email: '',
       password: '',
     },
   });
+
   const { errors } = form.formState;
+
   const handleLogin = (data: z.infer<typeof loginValidationSchema>) => {
     console.log(data);
   };
+
   return (
-    <form onSubmit={form.handleSubmit(handleLogin)}>
+    <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-6">
       <div className="relative flex flex-col gap-2">
-        <label className="text-md font-medium">Phone Number</label>
+        <label
+          htmlFor="email"
+          className="inline-block text-base font-medium text-neutral-700"
+        >
+          Email
+        </label>
         <input
-          {...form.register('phoneNumber')}
-          className="rounded-md bg-white p-2 focus:outline-none"
-          placeholder="Enter your phone number"
+          id="email"
+          type="email"
+          {...form.register('email')}
+          className="block w-full rounded-sm border-gray-200 px-3 py-2 text-base shadow-sm disabled:pointer-events-none disabled:opacity-50"
+          placeholder="Enter your email"
         />
-        {errors.phoneNumber && (
-          <span className="text-sm text-red-500">
-            {errors.phoneNumber?.message}
-          </span>
+        {errors.email && (
+          <span className="text-sm text-red-500">{errors.email?.message}</span>
         )}
       </div>
-      <div className="relative mt-4 flex flex-col gap-2">
-        <label className="text-md font-medium">Password</label>
+      <div className="relative flex flex-col gap-2">
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="password"
+            className="inline-block text-base font-medium text-neutral-700"
+          >
+            Password
+          </label>
+          <p className="text-primary text-base font-medium">Forgot Password?</p>
+        </div>
         <input
+          id="password"
+          type="password"
           {...form.register('password')}
-          className="rounded-md bg-white p-2 focus:outline-none"
-          placeholder="Enter your phone number"
+          className="block w-full rounded-sm border-gray-200 px-3 py-2 text-base shadow-sm disabled:pointer-events-none disabled:opacity-50"
+          placeholder="Enter your password"
         />
         {errors.password && (
           <span className="text-sm text-red-500">
@@ -48,18 +69,22 @@ const MyComponent = () => {
           </span>
         )}
       </div>
-      <Button type="submit" variant="primary" size="sm" className="mt-8 w-full">
+      <Button
+        type="submit"
+        variant="primary"
+        size="sm"
+        className="w-full text-base"
+      >
         Sign In
       </Button>
       <div className="mt-4 flex items-center justify-center gap-4">
-        <p className="">Forgot Password?</p>
         <div className="flex justify-start gap-2">
           <div className=" flex items-center gap-1">
-            <InfoIcon className="h-4 w-4 text-blue-500" />
+            <InfoIcon className="text-primary h-4 w-4" />
             <span className="text-sm">Help Center</span>
           </div>
           <div className=" flex items-center gap-1">
-            <DashboardIcon className="h-4 w-4 fill-blue-500" />
+            <PhoneIcon className="h-4 w-4 fill-blue-500" />
             <span className="text-sm">Contact Us : +95 9 797961628 </span>
           </div>
         </div>
