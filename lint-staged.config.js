6d247a4fe6cfa97e137c737223f7a9cef25baf45
MyatMinAung <myatminaung.dev@gmail.com>
2024-04-01 20:09:08 +0630

    update: type check lint-stage file emit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   // Target specific folders for type checking (if needed)
-  'apps/**/*.{ts,tsx}': () => ['pnpm nx affected --target=type-check'],
-  'packages/**/*.{ts,tsx}': () => ['pnpm nx affected --target=type-check'],
+  'apps/**/*.{ts,tsx}': () => ['pnpm nx affected --target=type-check --noEmit'],
+  'packages/**/*.{ts,tsx}': () => [
+    'pnpm nx affected --target=type-check --noEmit',
+  ],
 
   // Use lint-staged built-in globbing for staged files
   // '**/*.{js,ts,jsx,tsx}': ['pnpm nx affected:lint', 'pnpm nx format:write'],
