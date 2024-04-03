01a242c4ddc3c9bdc9fc097d11f539fe61c155ea
MyatMinAung <myatminaung.dev@gmail.com>
2024-04-03 12:24:58 +0630

    chore: restrict commitlint rule for type-case, subject-case

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,15 @@
 //-----------------------------------------------------------------------------------------------------------------------------------------------------
-// chore: used for miscellaneous changes that don't affect the main codebase(configuring development tools, setting up project-specific settings)
-// ci: Changes to our CI configuration files and scripts
-// docs: Documentation only changes
-// feat: A new feature
-// fix: A bug fix
-// perf: A code change that improves performance
-// refactor: A code change that neither fixes a bug nor adds a feature
-// style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-// test: Adding missing tests or correcting existing tests
-// update: Updates to the project's build process, CI configuration files, or other tools and libraries.
-// wip: Work in process
+// chore(scope): used for miscellaneous changes that don't affect the main codebase(configuring development tools, setting up project-specific settings)
+// ci(scope): Changes to our CI configuration files and scripts
+// docs(scope): Documentation only changes
+// feat(scope): A new feature
+// fix(scope): A bug fix
+// perf(scope): A code change that improves performance
+// refactor(scope): A code change that neither fixes a bug nor adds a feature
+// style(scope): Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+// test(scope): Adding missing tests or correcting existing tests
+// update(scope): Updates to the project's build process, CI configuration files, or other tools and libraries.
+// wip(scope): Work in process
 // -----------------------------------------------------------------------------------------------------------------------------------------------------
 
 module.exports = {
@@ -32,5 +32,8 @@ module.exports = {
         'wip',
       ],
     ],
+    'type-case': [2, 'always', ['lowerCase']],
+    'subject-case': [2, 'always', ['lower-case', 'kebab-case']],
+    'subject-empty': [2, 'never'],
   },
 };
