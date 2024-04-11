03695750a3f93d312c87dacdc338ac042b712afb
MyatMinAung <myatminaung.dev@gmail.com>
2024-04-11 10:18:22 +0630

    feat(role): add staff list and customize role and permission

--- apps/admin/src/app/(dashboard)/customers/[id]/page.tsx
+++ apps/admin/src/app/(dashboard)/customers/[id]/page.tsx
@@ -6,41 +6,38 @@ import CustomerStats from './components/customer-stats';
 const CustomerDetailPage = () => {
   return (
     <main className="space-y-5 p-4 lg:p-6">
-      <div className="space-y-1">
-        <ol className="flex items-center whitespace-nowrap">
-          <li className="inline-flex items-center">
-            <a
-              className="flex items-center text-base text-gray-500 hover:text-blue-600 focus:text-blue-600 focus:outline-none dark:focus:text-blue-500"
-              href="#"
-            >
-              Dashboard
-            </a>
+      <ol className="flex items-center whitespace-nowrap">
+        <li className="inline-flex items-center">
+          <a
+            className="flex items-center text-base text-gray-500 hover:text-blue-600 focus:text-blue-600 focus:outline-none dark:focus:text-blue-500"
+            href="#"
+          >
+            Dashboard
+          </a>
+          <ChevronIcon
+            direction="right"
+            className="mx-2 size-5 flex-shrink-0 overflow-visible text-gray-500 dark:text-neutral-600"
+          />
+        </li>
+        <li className="inline-flex items-center">
+          <a
+            className="flex items-center text-base text-gray-500 hover:text-blue-600 focus:text-blue-600 focus:outline-none dark:focus:text-blue-500"
+            href="#"
+          >
+            Customers
             <ChevronIcon
               direction="right"
               className="mx-2 size-5 flex-shrink-0 overflow-visible text-gray-500 dark:text-neutral-600"
             />
-          </li>
-          <li className="inline-flex items-center">
-            <a
-              className="flex items-center text-base text-gray-500 hover:text-blue-600 focus:text-blue-600 focus:outline-none dark:focus:text-blue-500"
-              href="#"
-            >
-              Customers
-              <ChevronIcon
-                direction="right"
-                className="mx-2 size-5 flex-shrink-0 overflow-visible text-gray-500 dark:text-neutral-600"
-              />
-            </a>
-          </li>
-          <li
-            className="inline-flex items-center truncate text-base font-medium text-neutral-700 dark:text-gray-200"
-            aria-current="page"
-          >
-            Adam Taylor
-          </li>
-        </ol>
-        <p className="text-lg font-medium text-neutral-700">Adam Taylor</p>
-      </div>
+          </a>
+        </li>
+        <li
+          className="inline-flex items-center truncate text-base font-medium text-neutral-700 dark:text-gray-200"
+          aria-current="page"
+        >
+          Adam Taylor
+        </li>
+      </ol>
 
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-4">
         <div className="col-span-1 space-y-4 lg:col-span-1">
--- apps/admin/src/app/(dashboard)/role-and-permission/page.tsx
+++ apps/admin/src/app/(dashboard)/role-and-permission/page.tsx
@@ -1,5 +1,33 @@
+import { ChevronIcon } from '@ecommerce/ui';
+import RoleList from './components/role-list';
+import AddStaff from './components/add-staff';
+
 const RoleAndPermission = () => {
-  return <div>Enter</div>;
+  return (
+    <main className="space-y-5 p-4 lg:p-6">
+      <div className="flex items-center justify-between">
+        <ol className="flex items-center whitespace-nowrap">
+          <li className="inline-flex items-center">
+            <p className="flex items-center text-base text-gray-500 focus:outline-none dark:focus:text-blue-500">
+              Dashboard
+            </p>
+            <ChevronIcon
+              direction="right"
+              className="mx-2 size-5 flex-shrink-0 overflow-visible text-gray-500 dark:text-neutral-600"
+            />
+          </li>
+          <li
+            className="inline-flex items-center truncate text-base font-medium text-neutral-700 dark:text-gray-200"
+            aria-current="page"
+          >
+            Role And Permission
+          </li>
+        </ol>
+        <AddStaff />
+      </div>
+      <RoleList />
+    </main>
+  );
 };
 
 export default RoleAndPermission;
